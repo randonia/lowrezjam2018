1b2f495e8c5ee3614ba662d97f9183374da3cd17
Ryan Andonian <ryanandonian@gmail.com>
2018-08-02 22:26:22 -0700

    Adds super simple terrain loading

--- dist/src/gamescene.js
+++ dist/src/gamescene.js
@@ -1,5 +1,5 @@
 var player;
-
+var _map;
 class GameScene extends BaseScene {
   preload() {
     console.info('Creating GameScene');
@@ -9,14 +9,23 @@ class GameScene extends BaseScene {
         frameHeight: 8
       }
     );
+    this.load.image('terrain', 'assets/tilesets/terrain.png');
+    this.load.tilemapTiledJSON('map', 'assets/tilemaps/world-1-1.json');
   }
   create() {
+    const map = this.make.tilemap({
+      key: 'map',
+    });
+
+    _map = map;
+    const tileset = map.addTilesetImage('ground', 'terrain');
+    const groundLayer = map.createStaticLayer('snow_floor', tileset, 0, 0);
+
     player = new Player({
       x: 0,
       y: 0,
       scene: this
     });
   }
-  update() {}
-  render() {}
+  update(time, delta) {}
 }
